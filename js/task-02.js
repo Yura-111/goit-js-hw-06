0550ac13fce5d95a697241f2d00fe0586cf19c82
Yura <104792276+Yura-111@users.noreply.github.com>
2022-09-09 00:34:52 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listId = document.querySelector('#ingredients');
+
+const makeManu = ingredients.map((ingredient) => {
+  const listManu = document.createElement('li');
+  listManu.classList.add('item');
+  listManu.textContent = `${ingredient}`;
+
+  return listManu;
+})
+
+listId.append(...makeManu);
+
+console.log(makeManu);
+
+
