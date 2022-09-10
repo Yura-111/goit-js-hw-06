84c07f77e02400f4114d44cc98dc0f5e21fda632
Yura <104792276+Yura-111@users.noreply.github.com>
2022-09-10 20:04:45 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyColor = document.querySelector('body');
+const btnChangeColor = document.querySelector('.change-color');
+const spanNameColor = document.querySelector('.color');
+
+const handlBtnRefClick = () => {  
+  let randomHexColor = getRandomHexColor();
+  bodyColor.style.backgroundColor = randomHexColor;
+  spanNameColor.textContent = randomHexColor;
+  console.log(randomHexColor);
+}
+
+btnChangeColor.addEventListener('click', handlBtnRefClick);
