030f894595c99087bd3b583d2242e13eef5a5f5b
Yura <104792276+Yura-111@users.noreply.github.com>
2022-09-10 18:11:37 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formRef = document.querySelector('.login-form')
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const {elements: {email, password}} = event.currentTarget;
+
+    if (email.value === "" || password.value === "") {
+        alert("Всі поля повинні бути заповнені!"); 
+        return 
+    }
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+    
+    event.currentTarget.reset();
+}
+
+formRef.addEventListener("submit", handleSubmit)
+
 
